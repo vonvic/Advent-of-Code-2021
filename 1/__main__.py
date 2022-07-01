90086ee761d4e512f94447f8654c333bacebe7c1
vonvic <40438077+vonvic@users.noreply.github.com>
2022-07-01 12:14:44 -0500

    Day 1 Part Two

--- 1/__main__.py
+++ 1/__main__.py
@@ -1,10 +1,10 @@
-def count_increases(depths: list):
-    '''Returns the number of times the depth increases from a previous
-    measurement.'''
+
+def count_increases(depths: list, width: int = 0):
+    '''Returns the number of times the sum of depths increases from a previous
+    measurement. The sum of depths is specified from the `width` argument.'''
     count = 0
     for i in range(1, len(depths)):
-        prev, curr = depths[i-1], depths[i]
-
+        prev, curr = sum(depths[i-1:i-1+width+1]), sum(depths[i:i+width+1])
         if (curr-prev) > 0: count += 1
     return count
 
@@ -12,5 +12,5 @@ if __name__ == '__main__':
     depths: list
     with open('input.txt', 'r') as f:
         depths = [int(line.strip()) for line in f.readlines()]
-    increase_count = count_increases(depths)
+    increase_count = count_increases(depths, 2)
     print(f'Increase count: {increase_count}')
