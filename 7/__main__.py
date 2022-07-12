87065da01fbc169e0de17a98917948ff0e8f1647
vonvic <40438077+vonvic@users.noreply.github.com>
2022-07-11 19:31:29 -0500

    Day 7 Part Two

--- 7/__main__.py
+++ 7/__main__.py
@@ -1,16 +1,29 @@
-def median(L: list):
-    '''Returns the median.'''
-    if len(L) % 2 == 0:
-        ind1, ind2 = len(L)//2-1, len(L)//2
-        return (L[ind1] + L[ind2])//2
-    else:
-        return L[len(L)//2-1]
+def get_target_one(L: list):
+    '''Returns the median of the list.'''
+    from statistics import median
+    return int(median(L))
 
-def calculate_fuel(L: list, target: int) -> int:
+def get_target_two(L: list) -> int:
+    '''Returns the mean of `L` rounded down to the nearest integer.'''
+    from statistics import mean
+    return int(mean(L))
+
+def calculate_fuel_one(L: list, target: int) -> int:
     '''Calculates the fuel by calcuating the absolute differences between each
     value in L and the target.'''
     from functools import reduce
     return reduce(lambda p, x: p + abs(target-x), L, 0)
+
+def calculate_fuel_two(L: list, target: int) -> int:
+    '''Calculates the fuel by getting the sum of triangle numbers, where each
+    triangle number is the defined by distance between each element in `L` and
+    `target`.'''
+    from functools import reduce
+    def f(p, x):
+        diff = int(abs(x-target)+1)
+        value = diff*(diff-1)//2
+        return p + value
+    return reduce(f, L, 0)
 
 def get_list(file: str):
     '''Returns the list of the initial state of crab horizontal positions.'''
@@ -19,9 +32,18 @@ def get_list(file: str):
 
 def main():
     crabs = sorted(get_list('input.txt'))
-    target = median(crabs)
-    fuel = calculate_fuel(crabs, target)
+    target = get_target_one(crabs)
+    fuel = calculate_fuel_one(crabs, target)
 
+    target_two = get_target_two(crabs)
+    fuel_two = calculate_fuel_two(crabs, target_two)
+
+    print('Part One')
     print(f'Fuel required to reposition at {target}: {fuel}')
+
+    print()
+
+    print('Part two')
+    print(f'Fuel required to reposition at {target_two}: {fuel_two}')
 
 if __name__ == '__main__': main()
