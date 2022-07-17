7fdb268c0caf1b41af12f8dd66600d172d567de8
vonvic <40438077+vonvic@users.noreply.github.com>
2022-07-16 19:24:58 -0500

    Day 14 Day Two

--- 14/__main__.py
+++ 14/__main__.py
@@ -1,6 +1,3 @@
-import enum
-from typing import Callable
-
 def get_template_and_rules(filename: str) -> tuple:
     '''Returns (template, rules) where the template is first line of the file
     called `filename`, and rules is the rest of the lines in that file
@@ -15,101 +12,46 @@ def get_template_and_rules(filename: str) -> tuple:
             rules[k] = v.strip()
     return (template, rules)
 
-# def run_pair_insertion(template: str, rules: dict) -> str:
-#     new_polymer = ''
-#     for i in range(len(template)-1):
-#         new_polymer += template[i] + rules[template[i:i+2]]
-#     return new_polymer + template[-1]
-
-# _OPT_SAVED = dict()
-# def _OPT(A: str, B: str, rules: dict, N: int) -> str:
-#     key = (A, B, N)
-#     if key in _OPT_SAVED: return _OPT_SAVED[key]
-#     if N == 0: return ''
-
-#     M: str = rules[A+B]
-#     s = _OPT(A, M, rules, N-1) + M + _OPT(M, B, rules, N-1)
-#     _OPT_SAVED[key] = s
-#     return s
-
-# def run_pair_insertion_N_times(template: str, rules: dict, N: int) -> str:
-#     polymer = ''
-
-#     for i in range(len(template)-1):
-#         print(i)
-#         A, B = template[i], template[i+1]
-#         polymer += A + _OPT(A, B, rules, N)
-#     print('complete')
-#     return polymer + template[-1]
-
-def get_frequency_table(s: str) -> dict:
-    freq_table = dict()
-    for c in s:
-        freq_table[c] = freq_table.get(c, 0) + 1
-    return freq_table
-
-# def get_part_one_answer(template: str, rules: dict, N: int) -> int:
-#     s = run_pair_insertion_N_times(template, rules, N)
-#     f = get_frequency_table(s)
-#     max_freq, min_freq = f[max(f, key=f.get)], f[min(f, key=f.get)]
-#     return max_freq - min_freq
-
-_OPT_SAVED: dict[tuple[str, str, int], Callable] = dict()
-def _OPT(A: str, B: str, rules: dict, N: int, freq: dict) -> str:
-    if N == 0: return ''
-
-    key = (A, B, N)
-    if key in _OPT_SAVED:
-        _OPT_SAVED[key]()
-        return
-
-    def f():
-        M: str = rules[A+B]
-        freq[M] = freq.get(M, 0) + 1
-        _OPT(A, M, rules, N-1, freq)
-        _OPT(M, B, rules, N-1, freq)
-        print
+def get_freq_table_after_running(template: str, rules: dict, N: int):
+    '''Returns the frequency of all the letters in the final string after
+    running `template` against `rules` for `N` times.'''
+    pairs_freq = {pair: 0 for pair in rules.keys()}
+    for i, v in enumerate(template[:-1]):
+        pair = v + template[i+1]
+        pairs_freq[pair] += 1
     
-    _OPT_SAVED[key] = f
-    f()
+    letter_freq = {letter : template.count(letter) for letter in set(template)}
+    for _ in range(N):
+        new_freq = {pair: 0 for pair in rules.keys()}
+        for pair, freq in pairs_freq.items():
+            if freq == 0: continue
 
-_OPT2_SAVED = dict()
-def _OPT2(A: str, B: str, rules: dict, N: int) -> str:
-    if N == 0: return ''
-    key = (A, B, N)
-    if key in _OPT2_SAVED: return _OPT2_SAVED[key]
+            c = rules[pair]
+            l, r = pair
+            new_freq[l+c] += freq
+            new_freq[c+r] += freq
 
-    M = rules[A+B]
-    L_key = (A, M, N-1)
-    L = _OPT2_SAVED[L_key] if L_key in _OPT2_SAVED else _OPT2(A, M, rules, N-1)
-    R_key = (M, B, N-1)
-    R = _OPT2_SAVED[R_key] if R_key in _OPT2_SAVED else _OPT2(M, B, rules, N-1)
-    s = L + M + R
-    _OPT2_SAVED[key] = s
-    return s
+            letter_freq[c] = letter_freq.get(c, 0) + freq
+        pairs_freq = new_freq
+    return letter_freq
 
-def get_most_and_least_freq_diff(template: str, rules: dict, N: int) -> int:
-    freq = dict()
-    for i, value in enumerate(template[:-1]):
-        A, B = value, template[i+1]
-        freq[A] = freq.get(A, 0) + 1
-        _OPT(A, B, rules, N, freq)
-    last = template[-1]
-    freq[last] = freq.get(last, 0) + 1
-    most, least = max(freq, key=freq.get), min(freq, key=freq.get)
-    return freq[most] - freq[least]
+def get_most_and_least_count(freq: dict) -> tuple[int, int]:
+    '''Returns the frequencies of the most frequent and least frequent items in
+    `freq`.'''
+    most_freq, least_freq = max(freq, key=freq.get), min(freq, key=freq.get)
+    return freq[most_freq], freq[least_freq]
 
 def main():
-    template, rules = get_template_and_rules('sample.txt')
-    N = 40
-    # part_one_s = _OPT2('N', 'N', rules, N)
-    # print('complete')
-    # part_one = get_most_and_least_freq_diff(template, rules, N)
-    # N = 40
-    # part_two = get_most_and_least_freq_diff(template, rules, N)
+    template, rules = get_template_and_rules('input.txt')
+    N = 10
+    freq = get_freq_table_after_running(template, rules, N)
+    most, least = get_most_and_least_count(freq)
+    print('Part One:', most-least)   
 
-    # print(part_one)
-    # print(part_two)
+    N = 40
+    freq = get_freq_table_after_running(template, rules, N)
+    most, least = most, least = get_most_and_least_count(freq)
+    print('Part Two:', most-least)
 
 if __name__ == '__main__':
     main()
