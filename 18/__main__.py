aced4f68b6fee90aaab33486271dcd9b72196ca8
vonvic <40438077+vonvic@users.noreply.github.com>
2022-07-27 11:49:37 -0500

    Implemented explode function

--- 18/__main__.py
+++ 18/__main__.py
@@ -1,30 +1,40 @@
 from __future__ import annotations
 from ast import literal_eval
 
+from numpy import isin
 class Node:
     def _check_value_type(value, expected_type) -> None:
         if not isinstance(value, expected_type):
             raise TypeError(f'{value} is not of type {expected_type}')
 
-    def __init__(self, parent: Node=None):
+    def __init__(self, parent: Node=None, depth: int=1):
         self._parent: Node = parent
+        self._depth = depth
     
     @property
-    def parent(self): return self._parent
+    def parent(self) -> Node: return self._parent
 
     @parent.setter
     def parent(self, value: Node):
         Node._check_value_type(value, Node)
         self._parent = value
 
+    @property
+    def depth(self) -> int: return self._depth
+
+    @depth.setter
+    def depth(self, value):
+        Node._check_value_type(value, int)
+        self._depth = value
+
 class ConnectingNode(Node):
-    def __init__(self, parent: Node=None, left: Node=None, right: Node=None):
-        super().__init__(parent)
+    def __init__(self, parent: Node=None, left: Node=None, right: Node=None, depth: int=1):
+        super().__init__(parent, depth)
         self._left: Node = left
         self._right: Node = right
     
     @property
-    def left(self): return self._left
+    def left(self) -> Node: return self._left
 
     @left.setter
     def left(self, value: Node):
@@ -32,7 +42,7 @@ class ConnectingNode(Node):
         self._left = value
 
     @property
-    def right(self): return self._right
+    def right(self) -> Node: return self._right
 
     @right.setter
     def right(self, value: Node):
@@ -43,12 +53,12 @@ class ConnectingNode(Node):
         return f'[{self._left}, {self._right}]'
 
 class ValueNode(Node):
-    def __init__(self, parent: Node=None, value: int=None):
-        super().__init__(parent)
+    def __init__(self, parent: Node=None, value: int=None, depth: int=1):
+        super().__init__(parent, depth)
         self._value: int = value
 
     @property
-    def value(self): return self._value
+    def value(self) -> int: return self._value
 
     @value.setter
     def value(self, new_value: int):
@@ -58,18 +68,71 @@ class ValueNode(Node):
     def __str__(self) -> str: return str(self._value)
 
 class LanternNumber:
-    def _build_tree(num_list: list) -> ConnectingNode:
-        L, R = num_list
-        L_node = LanternNumber._build_tree(L) if isinstance(L, list) else ValueNode(value=L)
-        R_node = LanternNumber._build_tree(R) if isinstance(R, list) is list else ValueNode(value=R)
+    MAX_DEPTH: int = 4
 
-        connecting = ConnectingNode(left=L_node, right=R_node)
+    def __init__(self, num_list: list):
+        self._root: ConnectingNode = self._build_tree(num_list)
+        self._leaf_parents: list[ConnectingNode] = []
+        self._gather_leaf_parents()
+        self._reduce()
+
+    def _build_tree(self, num_list: list, depth=1) -> ConnectingNode:
+        L, R = num_list
+        L_node = self._build_tree(L, depth+1) if isinstance(L, list) else ValueNode(value=L)
+        R_node = self._build_tree(R, depth+1) if isinstance(R, list) else ValueNode(value=R)
+
+        connecting = ConnectingNode(left=L_node, right=R_node, depth=depth)
         connecting.left.parent = connecting
         connecting.right.parent = connecting
         return connecting
 
-    def __init__(self, num_list: list):
-        self._root: ConnectingNode = LanternNumber._build_tree(num_list)
+    def _rec_gather_leaf_parents(self, node: Node):
+        if isinstance(node, ValueNode): return
+
+        self._rec_gather_leaf_parents(node.left)
+        if isinstance(node.left, ValueNode) or isinstance(node.right, ValueNode):
+            self._leaf_parents.append(node)
+        self._rec_gather_leaf_parents(node.right)
+
+    def _gather_leaf_parents(self):
+        self._rec_gather_leaf_parents(self._root)
+
+    def _reduce(self):
+        new_leaf_parents: list[ConnectingNode] = []
+        for index, node in enumerate(self._leaf_parents):
+            violates_depth = node.depth > LanternNumber.MAX_DEPTH
+            new_leaf_parent = self._explode(index, node) if violates_depth else node
+            new_leaf_parents.append(new_leaf_parent)
+        self._leaf_parents = new_leaf_parents
+
+    def _explode(self, index: int, node: ConnectingNode):
+        if index > 0:
+            left_parent = self._leaf_parents[index-1]
+            if isinstance(left_parent.right, ValueNode):
+                left_parent.right.value += node.left.value
+            else:
+                left_parent.left.value += node.left.value
+
+        if index < len(self._leaf_parents)-1:
+            right_parent = self._leaf_parents[index+1]
+            if isinstance(right_parent.left, ValueNode):
+                right_parent.left.value += node.right.value
+            else:
+                right_parent.right.value += node.right.value
+
+        parent: ConnectingNode = node.parent
+        new_node = ValueNode(value=0)
+        new_node.parent = parent
+
+        if parent.left is node:
+            parent.left = new_node
+        else:
+            parent.right = new_node
+
+        return parent
+
+    def _split(node: ValueNode):
+        pass
 
     def __str__(self) -> str: return str(self._root)
 
